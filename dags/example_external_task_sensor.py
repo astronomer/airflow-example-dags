5a9a8d7e84303ae7bfb3b76eae1a83e5dc7783ef
Viraj Parekh <viraj@astronomer.io>
2019-06-23 14:28:00 -0400

    Changes to demos

--- dags/example_external_task_sensor.py
+++ dags/example_external_task_sensor.py
@@ -5,36 +5,94 @@ from airflow.sensors.external_task_sensor import ExternalTaskSensor
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.models import Variable
+from airflow.operators.slack_operator import SlackAPIPostOperator
 
 import json
 
 
-jobs = json.loads(Variable.get('jobs'))
+jobs = [{
+        "source": "source_A",
+        "destination": "destination_A",
+        "image": "image_A"
+        }, {
+        "source": "source_B",
+        "destination": "destination_B",
+        "image": "image_B"
+        }, {
+        "source": "source_C",
+        "destination": "destination_C",
+        "image": "image_A"
+        }, {
+        "source": "source_C",
+        "destination": "destination_C",
+        "image": "image_C"
+        }, {
+        "source": "source_D",
+        "destination": "destination_D",
+        "image": "image_D"
+        }, {
+        "source": "source_E",
+        "destination": "destination_E",
+        "image": "image_E"
+        }, {
+        "source": "source_F",
+        "destination": "destination_F",
+        "image": "image_F"
+        }, {
+        "source": "source_G",
+        "destination": "destination_G",
+        "image": "image_G"
+        }, {
+        "source": "source_H",
+        "destination": "destination_H",
+        "image": "image_H"
+        }, {
+        "source": "source_I",
+        "destination": "destination_I",
+        "image": "image_I"
+        }, {
+        "source": "source_J",
+        "destination": "destination_J",
+        "image": "image_J"
+        }, {
+        "source": "source_K",
+        "destination": "destination_K",
+        "image": "image_K"
+        }]
 
 
 def create_dag(dag_id,
                schedule,
-               default_args):
-
-    def hello_world_py(*args):
-        print('Hello World')
+               default_args,
+               job):
 
     dag = DAG(dag_id,
               schedule_interval=schedule,
-              default_args=default_args)
+              default_args=default_args,
+              catchup=False,
+              max_active_runs=1)
 
     with dag:
-        for job in jobs:
-            t1 = ExternalTaskSensor(task_id='external_sensor_{0}'
-                                    .format(job['source']),
-                                    external_dag_id='etl_dag',
-                                    external_task_id='talend_etl_{0}'
-                                    .format(job['source']))
-            for i in range(0, 10):
-                t2 = PythonOperator(task_id='python_code_{0}'.format(i),
-                                    python_callable=hello_world_py)
+        start = DummyOperator(task_id='start')
+        t1 = ExternalTaskSensor(task_id='wait_for_talend_{0}'
+                                .format(job['source']),
+                                external_dag_id='etl_dag',
+                                external_task_id='talend_etl_{0}'
+                                .format(job['source']))
+        t2 = SlackAPIPostOperator(task_id='post_slack_{0}'.format(job['source']),
+                                  username='ETL',
+                                  slack_conn_id='slack_conn',
+                                  text="My job {0} finished".format(
+                                      job['source']),
+                                  channel='biz-cloud-billing')
 
-                t1 >> t2
+        start >> t1 >> t2
+
+        if job['source'] == 'source_B':
+            for i in range(0, 15):
+                m = DummyOperator(task_id='TeamB_alerting_logic_{0}'.format(i))
+
+                t2 >> m
 
     return dag
 
@@ -51,4 +109,5 @@ for job in jobs:
 
     globals()[dag_id] = create_dag(dag_id,
                                    schedule,
-                                   default_args)
+                                   default_args,
+                                   job)
--- dags/kube_pod.py
+++ dags/kube_pod.py
@@ -2,9 +2,13 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.models import Variable
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.slack_operator import SlackAPIPostOperator
+
 from airflow.operators.dummy_operator import DummyOperator
 import json
+import os
 
+NAMESPACE = os.getenv('HOSTNAME').split('-schedu')[0]
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -15,12 +19,63 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG('etl_dag',
-          schedule_interval='@once',
+dag = DAG(dag_id='etl_dag',
+          schedule_interval='@daily',
+          catchup=False,
           default_args=default_args)
 
-jobs = json.loads(Variable.get('jobs'))
+# jobs = Variable.get("jobs", deserialize_json=True)
+# set deseairlize to true here.
 
+jobs = [{
+        "source": "source_A",
+        "destination": "destination_A",
+        "image": "image_A"
+        }, {
+        "source": "source_B",
+        "destination": "destination_B",
+        "image": "image_B"
+        }, {
+        "source": "source_C",
+        "destination": "destination_C",
+        "image": "image_A"
+        }, {
+        "source": "source_C",
+        "destination": "destination_C",
+        "image": "image_C"
+        }, {
+        "source": "source_D",
+        "destination": "destination_D",
+        "image": "image_D"
+        }, {
+        "source": "source_E",
+        "destination": "destination_E",
+        "image": "image_E"
+        }, {
+        "source": "source_F",
+        "destination": "destination_F",
+        "image": "image_F"
+        }, {
+        "source": "source_G",
+        "destination": "destination_G",
+        "image": "image_G"
+        }, {
+        "source": "source_H",
+        "destination": "destination_H",
+        "image": "image_H"
+        }, {
+        "source": "source_I",
+        "destination": "destination_I",
+        "image": "image_I"
+        }, {
+        "source": "source_J",
+        "destination": "destination_J",
+        "image": "image_J"
+        }, {
+        "source": "source_K",
+        "destination": "destination_K",
+        "image": "image_K"
+        }]
 with dag:
 
     start = DummyOperator(task_id="start")
@@ -28,11 +83,30 @@ with dag:
     for job in jobs:
         k = KubernetesPodOperator(
             task_id="talend_etl_{0}".format(job['source']),
-            namespace='',
-            image="talend_job_{0}".format(job['image']),
-            labels={"foo": "bar"},
-            name="talend_etl_{0}".format(job['source']),
+            namespace='datarouter-glowing-lens-5847',
+            name='datarouter-glowing-lens-5847',
+            image="alpine:latest",
             in_cluster=True,
+            service_account='glowing-lens-5847-worker-serviceaccount',
             get_logs=True)
 
-        start >> k
+        t2 = SlackAPIPostOperator(task_id='post_slack_{0}'.format(job['source']),
+                                  username='ETL',
+                                  slack_conn_id='slack_conn',
+                                  text="My job {0} finished".format(
+                                      job['source']),
+                                  channel='biz-cloud-billing')
+
+        if job['source'] == 'source_B':
+            for i in range(0, 6):
+                m = DummyOperator(task_id='TeamB_alerting_logic_{0}'.format(i))
+
+                t2 >> m
+
+        if job['source'] == 'source_K':
+            for i in range(0, 3):
+                m = DummyOperator(task_id='Spark_Jobs_{0}'.format(i))
+
+                t2 >> m
+
+        start >> k >> t2
