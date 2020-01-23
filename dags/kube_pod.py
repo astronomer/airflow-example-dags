99e83b021c9b1e437c6368f162e9918f68a4de5f
vmpvmp94 <viraj@astronomer.io>
2020-01-23 10:57:54 -0500

    Added new example dag

--- dags/kube_pod.py
+++ dags/kube_pod.py
@@ -1,119 +1,47 @@
-# from airflow import DAG
-# from datetime import datetime, timedelta
-# from airflow.models import Variable
-# from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-# from airflow.operators.slack_operator import SlackAPIPostOperator
-# from airflow import configuration as conf 
-# from airflow.operators.dummy_operator import DummyOperator
-# import json
-# import os
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow import configuration as conf
 
-# namespace = conf.get('kubernetes', 'NAMESPACE')
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
-# if namespace !='default':
-#     config_file = '/usr/local/airflow'
-#     in_cluster=False
-# else:
-#     in_cluster=True
-#     config_file='None'
 
-# default_args = {
-#     'owner': 'airflow',
-#     'depends_on_past': False,
-#     'start_date': datetime(2019, 1, 1),
-#     'email_on_failure': False,
-#     'email_on_retry': False,
-#     'retries': 1,
-#     'retry_delay': timedelta(minutes=5),
-# }
+namespace = conf.get('kubernetes', 'NAMESPACE')
 
-# dag = DAG(dag_id='etl_dag',
-#           schedule_interval='*/15 * * * *',
-#           catchup=False,
-#           default_args=default_args)
+# This will detect the default namespace locally and read the 
+# environment namespace when deployed to Astronomer.
+if namespace =='default':
+    config_file = '/usr/local/airflow/include/.kube/config'
+    in_cluster=False
+else:
+    in_cluster=True
+    config_file=None
 
-# # jobs = Variable.get("jobs", deserialize_json=True)
-# # set deseairlize to true here.
+dag = DAG('example_kubernetes_pod',
+          schedule_interval='@once',
+          default_args=default_args)
 
-# jobs = [{
-#         "source": "source_A",
-#         "destination": "destination_A",
-#         "image": "image_A"
-#         }, {
-#         "source": "source_B",
-#         "destination": "destination_B",
-#         "image": "image_B"
-#         }, {
-#         "source": "source_C",
-#         "destination": "destination_C",
-#         "image": "image_A"
-#         }, {
-#         "source": "source_C",
-#         "destination": "destination_C",
-#         "image": "image_C"
-#         }, {
-#         "source": "source_D",
-#         "destination": "destination_D",
-#         "image": "image_D"
-#         }, {
-#         "source": "source_E",
-#         "destination": "destination_E",
-#         "image": "image_E"
-#         }, {
-#         "source": "source_F",
-#         "destination": "destination_F",
-#         "image": "image_F"
-#         }, {
-#         "source": "source_G",
-#         "destination": "destination_G",
-#         "image": "image_G"
-#         }, {
-#         "source": "source_H",
-#         "destination": "destination_H",
-#         "image": "image_H"
-#         }, {
-#         "source": "source_I",
-#         "destination": "destination_I",
-#         "image": "image_I"
-#         }, {
-#         "source": "source_J",
-#         "destination": "destination_J",
-#         "image": "image_J"
-#         }, {
-#         "source": "source_K",
-#         "destination": "destination_K",
-#         "image": "image_K"
-#         }]
-# with dag:
 
-#     start = DummyOperator(task_id="start")
+compute_resource = {'request_cpu': '200m', 'request_memory': '1Gi', 'limit_cpu': '200m', 'limit_memory': '1Gi'}
 
-#     for job in jobs:
-#         k = KubernetesPodOperator(
-#             task_id="talend_etl_{0}".format(job['source']),
-#             namespace=namespace,
-#             name='test_pod',
-#             image="alpine:latest",
-#             in_cluster=True,
-#             get_logs=True)
-
-#         t2 = SlackAPIPostOperator(task_id='post_slack_{0}'.format(job['source']),
-#                                   username='ETL',
-#                                   slack_conn_id='slack_conn',
-#                                   text="My job {0} finished".format(
-#                                       job['source']),
-#                                   channel='workflow_status')
-
-#         if job['source'] == 'source_B':
-#             for i in range(0, 6):
-#                 m = DummyOperator(task_id='TeamB_alerting_logic_{0}'.format(i))
-
-#                 t2 >> m
-
-#         if job['source'] == 'source_K':
-#             for i in range(0, 3):
-#                 m = DummyOperator(task_id='Spark_Jobs_{0}'.format(i))
-
-#                 t2 >> m
-
-#         start >> k >> t2
+with dag:
+    k = KubernetesPodOperator(
+        namespace=namespace,
+        image="hello-world",
+        labels={"foo": "bar"},
+        name="airflow-test-pod",
+        task_id="task-one",
+        in_cluster=in_cluster, # if set to true, will look in the cluster, if false, looks for file
+        cluster_context='docker-for-desktop', # is ignored when in_cluster is set to True
+        config_file=config_file,
+        resources=compute_resource,
+        is_delete_pod_operator=True,
+        get_logs=True)
